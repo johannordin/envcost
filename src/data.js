af572723e9519f0eb7483270a068c019dc43148e
johan nordin <johan.nordin@outlook.com>
2018-09-25 23:10:33 +0200

    real data, transitions.

--- src/data.js
+++ src/data.js
@@ -1,23 +1,33 @@
 export const data = [
   {
-    short: "🍔",
+    short: "🍴",
     name: "food",
     per: "kilo",
     items: [
-      { short: "🥩", name: "beef", value: 8 },
-      { short: "🥓", name: "pork", value: 5 },
-      { short: "🥔", name: "potato", value: 1 }
-    ]
+      { short: "🐄", name: "beef", value: 27 },
+      { short: "🐖", name: "pork", value: 6 },
+      { short: "🐔", name: "chicken", value: 2.4 },
+      { short: "🥚", name: "egg", value: 1.4 },
+      { short: "🥜", name: "beans", value: 0.4 },
+      { short: "🍚", name: "rice", value: 3 },
+      { short: "🍝", name: "pasta", value: 0.8 },
+      { short: "🥔", name: "potato", value: 0.1 },
+    ],
+    source: "WWF"
   },
   {
     short: "⛽",
     name: "transport",
     per: "km",
     items: [
-      { short: "🚗", name: "car", value: 4 },
-      { short: "⛵", name: "boat", value: 3 },
-      { short: "🚂", name: "train", value: 2 }
-    ]
+      { short: "🚗", name: "car", value: 85 },
+      { short: "🚌", name: "bus", value: 40 },
+      { short: "🚂", name: "train", value: 5 },
+      { short: "🛫", name: "airplane", value: 250 },
+      { short: "🚲", name: "bicycle", value: 1 },
+      
+    ],
+    source: "Trafikverket"
   }
 ];
 
